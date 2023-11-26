305c686562efb35e8b4f47983b7ec1788aabf4f3
Hoobbaboobba <xopoxopox@gmail.com>
2023-11-26 21:00:50 +0300

    commit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,17 +10,17 @@ interface Props {
 const Card = ({ src, text, altText }: Props) => {
   const [info, setInfo] = useState(false);
   return (
-    <div className="w-[280px] flex flex-col justify-center items-center relative rounded-lg">
+    <div className="w-[280px] mt-8 flex flex-col justify-center items-center relative rounded-lg">
       <div
         className={`${
-          info ? "translate-y-[0]" : "translate-y-[100%]"
-        } bg-light-orange h-[85%] bg-opacity-80 backdrop-blur-md p-6 transition duration-300 absolute bottom-[54px] left-0 flex justify-center items-center rounded-lg`}
+          info ? "translate-y-[0%]" : "translate-y-[100%]"
+        } bg-light-orange bg-opacity-80 h-[85%] backdrop-blur-md p-6 transition duration-300 absolute bottom-[25px] left-0 flex justify-center items-center rounded-lg`}
       >
         <p className={`${info ? "opacity-100" : "opacity-0"}`}>{text}</p>
       </div>
       <img src={src} alt={altText} width="280" height="370" />
       <div
-        className="w-full z-20 cursor-pointer bg-light-orange flex justify-center items-center py-4"
+        className="w-full z-20 cursor-pointer bg-light-orange flex justify-center items-center py-2"
         onClick={() => setInfo((event) => !event)}
       >
         <ArrowDownCircle
